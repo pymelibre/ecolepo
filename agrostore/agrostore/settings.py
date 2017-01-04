cd69493c76acb4868607c0c454600f50905d98c6
mcueto <cueto@live.cl>
2017-01-04 03:53:29 -0300

    Added basic template
    Added basic template with index view and url, using milligram, sass and static files

--- agrostore/agrostore/settings.py
+++ agrostore/agrostore/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'agrostore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
