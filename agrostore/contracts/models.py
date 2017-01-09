b5a83e12528f6b794d3c8aaf73dd5ad5d0d744e2
mcueto <cueto@live.cl>
2017-01-08 22:26:17 -0300

    Minor changes

--- agrostore/contracts/models.py
+++ agrostore/contracts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class BaseContract(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Contract(BaseContract):
+
+    def __str__(self):
+        return self.created_at
